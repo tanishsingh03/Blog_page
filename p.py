f5b2c125719d0904b8f37ca89c730d3c871f0ad4
Tanish Singh <luffylearning03@gmail.com>
2024-04-15 10:28:43 +0530

    1

---
+++ p.py
@@ -0,0 +1,89 @@
+# x=map(int,input().strip().split())
+# for i in x:
+#     if i%2==0:
+#         pass
+#     else:
+#         print(i)
+
+# i=0
+# while i < len(x):
+#     if i%2==0:
+#         pass
+#     else:
+#         print(i)
+
+# def fun(x):
+#     for i in range(1,x):
+#         if i%2==0:
+#             pass
+#         else:
+#             print(i)
+
+
+
+# def fun(int(input())):
+
+
+# x=map(int,input().strip().split)
+# y=map(int,input().strip().split)   
+
+
+
+
+ 
+
+# n = int(input("Enter number of elements : "))
+ 
+# z=map(int,input().strip().split())
+
+    
+# if len(z)==2:
+#     y=map(int,input().strip().split())
+#     if len(y)==2:
+     
+#      l=[]
+#      l.append(z)
+#      l.append(y)
+# print(l)            
+
+
+a=[1,2]
+b=[2,3]
+
+
+l=[]
+l.append(a)
+l.append(b)
+print(l)
+
+
+c=[3,4]
+v=[4,5]
+
+x=[]
+x.append(c)
+x.append(v)
+print(x)
+
+
+
+sum=0
+
+# for i in a:
+#     for j in c:
+#         sum = a[:1]+c[:1]
+# print(sum)        
+# for k in b:
+#     for t in v:
+#         o= b[:1]+v[:1]  
+
+# print([sum,o])        
+
+
+
+
+
+
+
+
+
